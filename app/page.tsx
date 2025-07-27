8df29efe9389afb81c535c4780ee0debd9710b59
Tushar Kumar <tushark81940@gmail.com>
2025-07-27 23:48:03 +0530

    new

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>My First Next Heading</h1>
+      <h1>My First Next Js Heading</h1>
     </div>
   );
 }
